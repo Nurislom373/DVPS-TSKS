1d15fd94a71a75b0ed4370935145dd385c71a8e4
Nurislom <nurislomx72@gmail.com>
2023-07-09 00:04:43 +0500

    add core jar

--- fluent-bot-lib/src/test/java/org/khasanof/crunch/CrunchTest.java
+++ fluent-bot-lib/src/test/java/org/khasanof/crunch/CrunchTest.java
@@ -5,12 +5,10 @@ import com.ezylang.evalex.Expression;
 import com.ezylang.evalex.data.EvaluationValue;
 import com.ezylang.evalex.parser.ParseException;
 import org.junit.jupiter.api.Test;
-import redempt.crunch.Crunch;
-import redempt.crunch.functional.EvaluationEnvironment;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Nurislom
@@ -18,14 +16,6 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 02.07.2023 19:02
  */
 public class CrunchTest {
-
-    @Test
-    void crunchTest() {
-        assertEquals(10, Crunch.evaluateExpression("$1 - $2", 20, 10));
-        assertEquals(2, Crunch.evaluateExpression("$1 / $2", 20, 10));
-        int someValue = 50;
-        assertEquals(47, Crunch.evaluateExpression("$1 - 3", someValue));
-    }
 
     @Test
     void evalTest() throws EvaluationException, ParseException {
